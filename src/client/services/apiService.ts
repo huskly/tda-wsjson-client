17d2a656aab1a3cb8aceaed56898c8b4685ac8e8
Felipe Lima <felipe.lima@gmail.com>
2023-06-16 00:34:09 -0700

    add new files

---
+++ src/client/services/apiService.ts
@@ -0,0 +1,17 @@
+export type ApiService =
+  | "login"
+  | "cancel_order"
+  | "chart"
+  | "order_events"
+  | "instrument_search"
+  | "optionSeries"
+  | "option_chain/get"
+  | "positions"
+  | "place_order"
+  | "quotes"
+  | "quotes/options"
+  | "user_properties"
+  | "alerts/create"
+  | "alerts/cancel"
+  | "alerts/subscribe"
+  | "alerts/lookup";
---
+++ src/client/services/loginMessageHandler.ts
@@ -0,0 +1,72 @@
+import WebSocketApiMessageHandler, {
+  newPayload,
+} from "./webSocketApiMessageHandler";
+import {
+  RawPayloadRequest,
+  RawPayloadResponse,
+  RawPayloadResponseItemHeader,
+} from "../tdaWsJsonTypes";
+import { ApiService } from "./apiService";
+
+export type RawLoginResponseBody = {
+  message?: string;
+  authenticationStatus: string;
+  authenticated: boolean;
+  forceLogout: boolean;
+  stalePassword: boolean;
+  userDomain: string;
+  userSegment: string;
+  userId: number;
+  userCdi: string;
+  userCode: string;
+  token: string;
+  schwabAccountMigrationValue: string;
+  permissions: {
+    isCryptoAllowed: boolean;
+    isFractionalQuantityAllowed: boolean;
+    isMandatoryAutoLockAllowed: boolean;
+    isSchwabIntegrationHubLinkAllowed: boolean;
+    isPlaceQuantityLinkOrdersAllowed: boolean;
+  };
+  quotePermissions: {
+    name: string;
+    isAllowed: boolean;
+    children: {
+      name: string;
+      isAllowed: boolean;
+      children: { name: string; isAllowed: boolean }[];
+    }[];
+  };
+};
+
+export type RawLoginResponse = {
+  payload: {
+    header: RawPayloadResponseItemHeader;
+    body: RawLoginResponseBody;
+  }[];
+};
+
+export default class LoginMessageHandler
+  implements WebSocketApiMessageHandler<string, boolean>
+{
+  parseResponse(message: RawPayloadResponse): boolean {
+    const [{ body }] = message.payload;
+    const { authenticationStatus } = body as RawLoginResponseBody;
+    return authenticationStatus === "OK";
+  }
+
+  buildRequest(accessToken: string): RawPayloadRequest {
+    return newPayload({
+      header: { service: "login", id: "login", ver: 0 },
+      params: {
+        accessToken,
+        domain: "TOS",
+        platform: "PROD",
+        token: "",
+        tag: "TOSWeb",
+      },
+    });
+  }
+
+  service: ApiService = "login";
+}
