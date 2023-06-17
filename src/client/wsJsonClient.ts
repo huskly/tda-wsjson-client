bea3a66a95220fc5f9cca6e13b4985c88b42c457
Felipe Lima <felipe.lima@gmail.com>
2023-06-16 22:39:19 -0700

    add test for message handler registration

--- src/client/wsJsonClient.ts
+++ src/client/wsJsonClient.ts
@@ -298,7 +298,10 @@ export default class WsJsonClient {
       .promise() as Promise<UserPropertiesResponse>;
   }
 
-  private dispatch<Req, Res>(
+  // This method should probably be kept private but is called in tests. There's no real reason for anyone to call this
+  // directly, except maybe if there's a new service that's not yet implemented by one of the existing MessageHandlers
+  // that you want to create and instantiate yourself. Use at your own risk.
+  dispatch<Req, Res>(
     handler: WebSocketApiMessageHandler<Req, Res>,
     args: Req
   ): Observable<ParsedWebSocketResponse> {
