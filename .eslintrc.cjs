8d25494551c766b903f5e7afe824ec777777eded
Felipe Lima <felipe.lima@gmail.com>
2023-12-10 21:40:23 -0300

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,14 @@ module.exports = {
   rules: {
     "@typescript-eslint/switch-exhaustiveness-check": "error",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn"
+    "@typescript-eslint/no-unused-vars": "warn",
   },
   ignorePatterns: ["src/test/**/*"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
       },
     },
   ],
